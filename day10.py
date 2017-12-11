c47175367cca37d4f975e66c6998e9bc879f5e5a
mastercake10 <mczocker@gmx.net>
2017-12-11 19:26:57 +0100

    Enhanced hex to string conversation

--- day10.py
+++ day10.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python2
 
 input = "31,2,85,1,80,109,35,63,98,255,0,13,105,254,128,33"
 
@@ -19,6 +19,11 @@ def calcKnotHash(lengths, rounds=1):
             skip_size += 1
     return ran
 
+def formatHexDigit(digit):
+    if len(digit) == 3:
+        return "0" + digit[2:3]
+    return digit[2:4];
+
 ran = calcKnotHash(map(int, input.split(",")))
 print("Output part1: " + str(ran[0] * ran[1]))
 
@@ -28,6 +33,6 @@ densehash = []
 
 for i in range(16):
     densehash.append(reduce(lambda x,y: x^y,ran[i*16:i*16+16]))
-finalhash = "".join(map(lambda x: x.split("x")[1], map(hex, densehash)))
+finalhash = "".join(map(lambda x: formatHexDigit(x), map(hex, densehash)))
 
 print("Output part2: " + finalhash)
